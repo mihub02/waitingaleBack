22f75308b1557c55f737fd097fba5973454b1404
mihub02 <jay06holi@gmail.com>
2026-02-24 15:17:13 +0900

    [Feat] 게시판 글 작성 기능

--- src/main/java/org/example/prac01/board/BoardController.java
+++ src/main/java/org/example/prac01/board/BoardController.java
@@ -1,4 +1,35 @@
 package org.example.prac01.board;
 
+
+import lombok.RequiredArgsConstructor;
+import org.example.prac01.board.model.BoardDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/board")
+@RestController
+@RequiredArgsConstructor
 public class BoardController {
-}
+    private final BoardService boardService;
+
+    @PostMapping("/reg")
+    public ResponseEntity register(@RequestBody BoardDto.Reg dto) {
+        boardService.register(dto);
+        return ResponseEntity.ok("성공");
+    }
+
+    @GetMapping("/read/{idx}")
+    public ResponseEntity read(@PathVariable Long idx) {
+        BoardDto.Res dto = boardService.read(idx);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity list() {
+        List<BoardDto.Res> dto = boardService.list();
+        return ResponseEntity.ok(dto);
+    }
+
+    }
--- src/main/java/org/example/prac01/board/model/BoardDto.java
+++ src/main/java/org/example/prac01/board/model/BoardDto.java
@@ -1,4 +1,52 @@
 package org.example.prac01.board.model;
 
+import lombok.*;
+
 public class BoardDto {
+    @Getter
+    public static class Reg {
+        private String title;
+        private String contents;
+
+        public Board toEntity() {
+            return Board.builder()
+                    .title(this.title)
+                    .contents(this.contents)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Res {
+        private Long idx;
+        private String title;
+        private String contents;
+//        private List<ReplyDto.Read> replyList;
+
+        public static Res from(Board entity) {
+            return Res.builder()
+                    .idx(entity.getIdx())
+                    .title(entity.getTitle())
+                    .contents(entity.getContents())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+
+    public static class Read {
+        private int idx;
+        private String title;
+        private String contents;
+        private int ridx;
+        private String rcontents;
+    }
 }
