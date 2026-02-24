3e6c8561505ce8b904ca2dfc7d2c937a8700d108
mihub02 <jay06holi@gmail.com>
2026-02-24 23:08:45 +0900

    [fix] 잘못 연결된 상세/목록 조회 로직 수정 및 코드 설명 주석 보강

--- src/main/java/org/example/prac01/board/BoardController.java
+++ src/main/java/org/example/prac01/board/BoardController.java
@@ -10,30 +10,41 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(
+        origins = "http://localhost:5173/",
+        allowCredentials = "true"
+)
+
 @RequestMapping("/board")
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
+    // 게시글 등록
     @PostMapping("/reg")
     public ResponseEntity register(@RequestBody BoardDto.Reg dto) {
         boardService.register(dto);
         return ResponseEntity.ok("성공");
     }
 
-    @GetMapping("/read/{idx}")
-    public ResponseEntity read(@PathVariable Long idx) {
-        BoardDto.Res dto = boardService.read(idx);
-        return ResponseEntity.ok(dto);
-    }
-
+    // 게시글 목록 조회
     @GetMapping("/list")
     public ResponseEntity list() {
         List<BoardDto.Res> dto = boardService.list();
         return ResponseEntity.ok(dto);
     }
 
+    // 게시글 상세 조회
+    @GetMapping("/read/{idx}")
+    public ResponseEntity read(@PathVariable Long idx) {
+        BoardDto.Res dto = boardService.read(idx);
+        return ResponseEntity.ok(dto);
+    }
+
+
+    // 게시글 수정
     @PutMapping("/update/{idx}")
     public ResponseEntity update(@PathVariable Long idx,@RequestBody BoardDto.Reg dto) {
         boardService.update(idx, dto);
